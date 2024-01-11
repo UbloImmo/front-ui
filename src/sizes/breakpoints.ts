a931d59d6c94736b03d6ec2bb75bd186f70a13d4
blksnk <art.jnveigel@gmail.com>
2024-01-11 17:22:19 +0100

    feat(spacings): breakpoints

---
+++ src/sizes/breakpoints.ts
@@ -0,0 +1,17 @@
+import { transformObject } from "@ubloimmo/front-util";
+
+export const breakpoints = {
+  XXS: 320,
+  XS: 420,
+  SM: 640,
+  MD: 768,
+  LG: 1024,
+  XL: 1280,
+  XXL: 1536,
+} as const;
+
+export const breakpointsPx = transformObject<
+  keyof typeof breakpoints,
+  number,
+  string
+>(breakpoints, (value) => `${value}px`) as const;
