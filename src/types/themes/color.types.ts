5d954dc3857c9a59ae0294943c2d084c49bfacae
blksnk <art.jnveigel@gmail.com>
2024-01-15 16:34:54 +0100

    chore: move palette typings to own file

--- src/types/themes/color.types.ts
+++ src/types/themes/color.types.ts
@@ -1,18 +1,5 @@
 import { Enum } from "@/types/global/global.types";
 
-const paletteColors = [
-  "primary",
-  "warning",
-  "pending",
-  "success",
-  "info",
-  "white",
-  "black",
-  "gray",
-] as const;
-
-export type PaletteColor = Enum<typeof paletteColors>;
-
 export type RgbaColorStr =
   | `rgba(${number}, ${number}, ${number}, ${number})`
   | `rgba(${number},${number},${number},${number})`;
