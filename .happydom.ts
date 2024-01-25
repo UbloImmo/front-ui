a5c20861990c93a92e08014544166e907dde8f4f
blksnk <art.jnveigel@gmail.com>
2024-01-25 16:43:14 +0100

    chore: document .happydom.ts

--- .happydom.ts
+++ .happydom.ts
@@ -1,3 +1,7 @@
 import { GlobalRegistrator } from "@happy-dom/global-registrator";
 
+/**
+ * Exposes dom global variables to bun:test and thus
+ * enables `@testing-library/react` to function without issues
+ */
 GlobalRegistrator.register();
