1858130760be093b8d9fc8593bb9f5e90b62c165
blksnk <art.jnveigel@gmail.com>
2024-01-30 15:26:26 +0100

    feat: heading & text components

---
+++ src/components/Text/Text.component.tsx
@@ -0,0 +1,20 @@
+import { styled } from "styled-components";
+import { TextProps } from "src";
+import {
+  buildTypographyStyle,
+  defaultTypographyProps,
+} from "../../typography/typography.styles";
+
+const defaultTextProps: Required<TextProps> = {
+  ...defaultTypographyProps,
+  size: "m",
+} as const;
+/**
+ * Renders a text component
+ *
+ * @param {TextProps} props - Text component props
+ */
+export const Text = styled.span<TextProps>`
+  display: inline-block;
+  ${buildTypographyStyle(defaultTextProps)}
+`;
