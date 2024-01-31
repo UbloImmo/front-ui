fa7d748fea7e5d1081bb895ddd2e297ae0fab3c8
blksnk <art.jnveigel@gmail.com>
2024-01-31 15:48:43 +0100

    chore: add css fr and global css length types

--- src/types/global/css.types.ts
+++ src/types/global/css.types.ts
@@ -1,3 +1,5 @@
+import { SpacingLabel } from "..";
+
 export type CssPx = `${number}px`;
 
 export type CssRem = `${number}rem`;
@@ -7,3 +9,9 @@ export type CssVarName = `--${string}`;
 export type CssVar<TValue extends string> = `${CssVarName}: ${TValue};`;
 
 export type CssVarUsage = `var(${CssVarName})`;
+
+export type CssFr = `${number}fr`;
+
+export type CssLength = SpacingLabel | CssRem | CssPx | CssFr | number;
+
+export type CssLengthUsage = CssPx | CssRem | CssVarUsage | CssFr;
