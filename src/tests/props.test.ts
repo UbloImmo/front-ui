01816d4dbee357a3807d4ba36688e68e8248297d
blksnk <art.jnveigel@gmail.com>
2024-01-31 16:19:41 +0100

    feat: utility function to merge default props

---
+++ src/tests/props.test.ts
@@ -0,0 +1,45 @@
+import { Nullable } from "@ubloimmo/front-util";
+import { describe, it, expect } from "bun:test";
+import { mergeDefaultProps } from "../utils";
+
+type TestProps = {
+  a?: string;
+  b: Nullable<string>;
+  c: number;
+};
+
+type TestDefaultProps = Required<TestProps>;
+
+const defaultProps: TestDefaultProps = {
+  a: "a",
+  b: "b",
+  c: 3,
+};
+
+describe("prop utils", () => {
+  describe("mergeDefaultProps", () => {
+    it("should be a function", () => {
+      expect(mergeDefaultProps).toBeDefined();
+      expect(mergeDefaultProps).toBeFunction();
+      expect(() => mergeDefaultProps(defaultProps)).not.toThrow();
+    });
+
+    it("should return default props when no props are provided", () => {
+      expect(mergeDefaultProps(defaultProps)).toBeObject();
+      expect(mergeDefaultProps(defaultProps)).not.toBeEmptyObject();
+      expect(mergeDefaultProps(defaultProps)).toContainKeys(["a", "b", "c"]);
+      expect(mergeDefaultProps(defaultProps)).toEqual(defaultProps);
+    });
+
+    it("should return default props when props is an empty object", () => {
+      expect(mergeDefaultProps(defaultProps, {})).toBeObject();
+      expect(mergeDefaultProps(defaultProps, {})).not.toBeEmptyObject();
+      expect(mergeDefaultProps(defaultProps, {})).toContainKeys([
+        "a",
+        "b",
+        "c",
+      ]);
+      expect(mergeDefaultProps(defaultProps, {})).toEqual(defaultProps);
+    });
+  });
+});
