91761a324019cb5ce5ed9f1fb488f9a6a5396b73
blksnk <art.jnveigel@gmail.com>
2024-01-31 16:08:01 +0100

    chore: document new css types

--- src/types/global/css.types.ts
+++ src/types/global/css.types.ts
@@ -1,17 +1,75 @@
 import { SpacingLabel } from "..";
 
+/**
+ * A CSS `px` length
+ *
+ * @example
+ * const pixels: CssPx = "100px";
+ */
 export type CssPx = `${number}px`;
 
+/**
+ * A CSS `rem` length
+ *
+ * @example
+ * const rems: CssRem = "100rem";
+ */
 export type CssRem = `${number}rem`;
 
-export type CssVarName = `--${string}`;
-
-export type CssVar<TValue extends string> = `${CssVarName}: ${TValue};`;
-
-export type CssVarUsage = `var(${CssVarName})`;
-
+/**
+ * A CSS `fr` length
+ *
+ * @example
+ * const fraction: CssFr = "2fr";
+ */
 export type CssFr = `${number}fr`;
 
+/**
+ * Any supported CSS length declaration
+ *
+ * Can be any of the following:
+ * - {@link CssPx}: `100px`, `200px`, `300px`, etc...
+ * - {@link CssRem}: `100rem`, `200rem`, `300rem`, etc...
+ * - {@link CssFr}: `1fr`, `2fr`, `3fr`, etc...
+ * - {@link SpacingLabel}: `s05`, `s1`, `s2`, `s3`, etc...
+ * - {@link number}: `100`, `200`, `300`, etc...
+ */
 export type CssLength = SpacingLabel | CssRem | CssPx | CssFr | number;
 
+/**
+ * Any supported CSS length usage
+ *
+ * Can be any of the following:
+ * - {@link CssPx}: `100px`, `200px`, `300px`, etc...
+ * - {@link CssRem}: `100rem`, `200rem`, `300rem`, etc...
+ * - {@link CssFr}: `1fr`, `2fr`, `3fr`, etc...
+ * - {@link CssVarUsage}: `var(--s05)`, `var(--s1)`, `var(--s2)`, `var(--s3)`, etc...
+ */
 export type CssLengthUsage = CssPx | CssRem | CssVarUsage | CssFr;
+
+/**
+ * A CSS variable name
+ *
+ * @example
+ * const primayBase: CssVarName = "--primary-base";
+ */
+export type CssVarName = `--${string}`;
+
+/**
+ * A CSS Variable rule declaration
+ *
+ * @example
+ * const primaryBaseName: CssVarName = "--primary-base";
+ * const primaryBaseColor: HexColor = "#467861"
+ * const primaryBaseRule: CssVar = "--primary-base: #467861;"
+ */
+export type CssVar<TValue extends string> = `${CssVarName}: ${TValue};`;
+
+/**
+ * A CSS Variable usage
+ *
+ * @example
+ * const primaryBaseName: CssVarName = "--primary-base";
+ * const primaryBaseUsage: CssVarUsage = "var(--primary-base)"
+ */
+export type CssVarUsage = `var(${CssVarName})`;
