14157a138b02fd02936f4126b7730c6900b022a6
blksnk <art.jnveigel@gmail.com>
2024-02-01 15:55:40 +0100

    fix: spacing label type

--- src/types/themes/sizes/spacing.types.ts
+++ src/types/themes/sizes/spacing.types.ts
@@ -4,7 +4,7 @@ export const UNIT_PX = 4 as const;
 
 export const SPACING_PREFIX = "s" as const;
 
-export type SpacingLabel = `${typeof SPACING_PREFIX}${number}`;
+export type SpacingLabel = `${typeof SPACING_PREFIX}-${number}`;
 
 export type Spacings = {
   [k: SpacingLabel]: CssRem;
