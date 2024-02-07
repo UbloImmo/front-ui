547777e5ccea7dda948ec49bc6a06d7f6f6031f3
blksnk <art.jnveigel@gmail.com>
2024-02-07 10:22:25 +0100

    fix: add jsdoc

--- scripts/svg/svg.utils.ts
+++ scripts/svg/svg.utils.ts
@@ -1,6 +1,12 @@
 import { isObject, isString } from "@ubloimmo/front-util";
 import { BootstrapIconFile, CustomIconFile } from "./svg.types";
 
+/**
+ * Checks if the given value is a BootstrapIconFile.
+ *
+ * @param {BootstrapIconFile | CustomIconFile} value - the value to check
+ * @return {boolean} true if the value is a BootstrapIconFile, false otherwise
+ */
 export const isBootstrapIconFile = (
   value: BootstrapIconFile | CustomIconFile
 ): value is BootstrapIconFile => {
--- src/components/Icon/Icon.component.tsx
+++ src/components/Icon/Icon.component.tsx
@@ -10,6 +10,17 @@ const defaultIconProps: DefaultIconProps = {
   name: "Circle",
 };
 
+/**
+ * Renders the appropriate icon component based on the provided props.
+ *
+ * This function first determines the icon name based on the provided props,
+ * then looks up the corresponding icon component. If the icon component is
+ * found, it is rendered with the provided size and color props. If the icon
+ * component is not found, null is returned.
+ *
+ * @param {IconProps} props - The props for the icon component.
+ * @return {JSX.Element | null} The rendered icon component or null if the icon component is not found.
+ */
 export const Icon = (props: IconProps) => {
   const { name } = useMemo(
     () => mergeDefaultProps(defaultIconProps, props),
