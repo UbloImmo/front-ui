eb81ba1ae2cbd37d68e5198afd2661d826fc5797
blksnk <art.jnveigel@gmail.com>
2024-02-27 18:34:59 +0100

    feat(theme): use theme overrides

--- src/themes/provider/theme.provider.tsx
+++ src/themes/provider/theme.provider.tsx
@@ -1,10 +1,13 @@
 import { ThemeProvider as StyledThemeProvider } from "styled-components";
-import { ReactNode, useMemo } from "react";
+import { ReactNode, useMemo, useState, useEffect } from "react";
 import { buildTheme } from "../theme";
 import { GlobalStyle } from "./globalStyle";
+import type { GetThemeOverridesFn, Nullable, ThemeOverride } from "../../types";
+import { isNullish } from "@ubloimmo/front-util";
 
 type ThemeProviderProps = {
   children: ReactNode;
+  getOverridesFn?: GetThemeOverridesFn;
 };
 
 /**
@@ -13,9 +16,27 @@ type ThemeProviderProps = {
  * @param {ReactNode} children - The child components to apply the theme to.
  * @return {JSX.Element} The styled theme provider component with the provided theme.
  */
-export const ThemeProvider = ({ children }: ThemeProviderProps) => {
-  // TODO: get current client org from Kaffy
-  const theme = useMemo(() => buildTheme(), []);
+export const ThemeProvider = ({
+  children,
+  getOverridesFn,
+}: ThemeProviderProps): JSX.Element => {
+  const [overrides, setOverrides] = useState<Nullable<ThemeOverride>>();
+
+  useEffect(() => {
+    if (isNullish(getOverridesFn)) return;
+    /**
+     * Fetches overrides asynchronously and sets the overrides data.
+     *
+     * @return {Promise<void>}
+     */
+    const fetchOverrides = async () => {
+      const data = await getOverridesFn();
+      setOverrides(data);
+    };
+    fetchOverrides();
+  }, [getOverridesFn]);
+
+  const theme = useMemo(() => buildTheme(overrides), [overrides]);
 
   return (
     <StyledThemeProvider theme={theme}>
