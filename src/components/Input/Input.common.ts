f6665c18e0627464a10646254e0f9951f68fff4a
blksnk <art.jnveigel@gmail.com>
2024-03-18 12:50:03 +0100

    chore: move defaut common input props to separate file to avoir circular deps

---
+++ src/components/Input/Input.common.ts
@@ -0,0 +1,7 @@
+import type { DefaultCommonInputProps } from "./Input.types.ts";
+
+export const defaultCommonInputProps: DefaultCommonInputProps = {
+  error: false,
+  disabled: false,
+  placeholder: "",
+};
