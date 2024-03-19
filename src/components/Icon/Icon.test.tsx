6177c1258085a7a9cf6b9077f46212e735203de8
blksnk <art.jnveigel@gmail.com>
2024-03-19 15:46:35 +0100

    chore(wip): icon tests

--- src/components/Icon/Icon.test.tsx
+++ src/components/Icon/Icon.test.tsx
@@ -1,7 +1,9 @@
-import { describe, it, expect, afterEach } from "bun:test";
+import { describe, it, expect, afterEach, mock } from "bun:test";
 import { render, cleanup } from "@testing-library/react";
 import { Icon } from "./Icon.component";
 import { ThemeProvider } from "../../themes";
+
+// mock global console object to list to calls
 
 describe("Icon", () => {
   it("should be a component", () => {
@@ -9,12 +11,29 @@ describe("Icon", () => {
     expect(Icon).toBeFunction();
   });
 
-  it.todo("should render even with missing props", () => {
+  it.todo("should render", () => {
     const { getByTestId } = render(
       <ThemeProvider>
+        <Icon name="Circle" />
+      </ThemeProvider>
+    );
+    const icon = getByTestId("icon");
+    expect(getByTestId("icon")).toBeDefined();
+    const iconStyle = window.getComputedStyle(icon);
+    expect(iconStyle).toBeObject();
+  });
+
+  it.todo("should render with a warning when missing its `name` prop", () => {
+    // mock global console object to list to calls
+    global.console.warn = mock(global.console.warn);
+    const { getByTestId } = render(
+      <ThemeProvider>
+        {/* eslint-disable-next-line @typescript-eslint/ban-ts-comment */}
+        {/* @ts-ignore Need to ignore in order to test missing prop */}
         <Icon />
       </ThemeProvider>
     );
+    expect(global.console.warn).toHaveBeenCalled();
     expect(getByTestId("icon")).toBeDefined();
   });
 
