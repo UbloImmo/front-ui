0e1357ce245176bb856fa45d728f88c9035bd47d
Bulle Ouvrard <bulle.ouvrard@gmail.com>
2024-03-20 11:31:24 +0100

    util: added ValueMap type

--- src/types/global/global.types.ts
+++ src/types/global/global.types.ts
@@ -8,3 +8,15 @@ export * from "@ubloimmo/front-util/lib/types";
  * type MyEnum = Enum<typeof myEnumValues>;
  */
 export type Enum<T extends string[] | readonly string[]> = T[number];
+
+/**
+ * An object that must contain all keys in TKeys,
+ * where all keys in TKeys must match TValue's types
+ *
+ * @template {string} TKeys - The object's keys. A string union
+ * @template {unknown} TValues - The object's values for every key in {@link TKeys}.
+ *
+ */
+export type ValueMap<TKeys extends string, TValue> = {
+  [TKey in TKeys]: TValue;
+};
