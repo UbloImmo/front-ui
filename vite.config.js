2646697f398b8c656193912f85a6bbcd74a1912b
blksnk <art.jnveigel@gmail.com>
2024-03-25 11:51:42 +0100

    chore: path aliases

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
+/** @type {import('vite').UserConfig} */
 export default {
   build: {
     rollupOptions: {
       external: ["bun:test", "*.test.ts", "*.test.tsx"],
+    },
+  },
+  resolve: {
+    alias: {
+      "@/*": "src/*",
+      "@docs/*": "docs/*",
+      "@types/*": "src/types/*",
+      "@utils/*": "src/utils/*",
+      "@components/*": "src/components/*",
     },
   },
 };
