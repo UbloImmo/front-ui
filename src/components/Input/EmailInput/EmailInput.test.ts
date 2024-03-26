773104c579e8e9448b1aa50337741f84d5752378
blksnk <art.jnveigel@gmail.com>
2024-03-26 10:18:40 +0100

    chore: tests

--- src/components/Input/EmailInput/EmailInput.test.ts
+++ src/components/Input/EmailInput/EmailInput.test.ts
@@ -1,6 +1,8 @@
-import { describe } from "bun:test";
-import { componentTestFactory } from "src/tests";
+import { describe, it, expect } from "bun:test";
+import { componentTestFactory, testPrimitives } from "src/tests";
 import { EmailInput, defaultEmailInputProps } from "./EmailInput.component";
+import { isEmailString } from "./EmailInput.utils";
+import { objectValues } from "@ubloimmo/front-util";
 
 describe("Email Input", () => {
   componentTestFactory(
@@ -9,4 +11,38 @@ describe("Email Input", () => {
     EmailInput,
     defaultEmailInputProps
   );
+
+  describe("isEmailString", () => {
+    it("should be a function", () => {
+      expect(isEmailString).toBeDefined();
+      expect(isEmailString).toBeFunction();
+    });
+    it("should never throw", () => {
+      objectValues(testPrimitives).forEach((primitive) => {
+        expect(() => isEmailString(primitive)).not.toThrow();
+      });
+    });
+    it("should always return a boolean", () => {
+      objectValues(testPrimitives).forEach((primitive) => {
+        expect(isEmailString(primitive)).toBeBoolean();
+      });
+    });
+    it("should return false for invalid values", () => {
+      objectValues(testPrimitives).forEach((primitive) => {
+        expect(isEmailString(primitive)).toBeFalse();
+      });
+    });
+    it("should return true for a kinda valid email", () => {
+      const validEmails = [
+        "example@mail.fr",
+        "a@aa.co",
+        "a3@tes5t.www1",
+        "example1.878676arta@side.extension",
+      ];
+
+      validEmails.forEach((email) => {
+        expect(isEmailString(email)).toBeTrue();
+      });
+    });
+  });
 });
--- src/components/Input/EmailInput/EmailInput.utils.ts
+++ src/components/Input/EmailInput/EmailInput.utils.ts
@@ -2,9 +2,11 @@ import { isString } from "@ubloimmo/front-util";
 import { Email } from "../../../types";
 
 // example.subdomain@domain.sub.com
-const emailRegex = /[\w.\d]+@[\w.\d]+\.[\w.\d]+/g;
+const emailRegex = /[\w.\d]+@[\w.\d]+\.[\w.\d]+/;
 
 export const isEmailString = (value: unknown): value is Email => {
+  // reset regex before testing
+  emailRegex.lastIndex = 0;
   // condition (string)
   if (!isString(value)) return false;
   // condition (email)
