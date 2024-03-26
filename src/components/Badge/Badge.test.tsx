e8cf661f6500e679ffed0df74be76ebbf7d27b1a
blksnk <art.jnveigel@gmail.com>
2024-03-26 12:56:23 +0100

    chore: remove duplicate describe

--- src/components/Badge/Badge.test.tsx
+++ src/components/Badge/Badge.test.tsx
@@ -1,17 +1,15 @@
-import { describe, expect } from "bun:test";
+import { expect } from "bun:test";
 import { testComponentFactory } from "src/tests";
 import { Badge } from "./Badge.component";
 
-describe("Badge", () => {
-  testComponentFactory("Badge", Badge, {
-    props: Badge.defaultProps,
-    tests: [
-      {
-        name: "should render",
-        test: ({ queryByTestId }) => {
-          expect(queryByTestId("badge")).toBeDefined();
-        },
+testComponentFactory("Badge", Badge, {
+  props: Badge.defaultProps,
+  tests: [
+    {
+      name: "should render",
+      test: ({ queryByTestId }) => {
+        expect(queryByTestId("badge")).toBeDefined();
       },
-    ],
-  });
+    },
+  ],
 });
