15f1a07cf273977e2e7a4f49aeaa82abdcfddd05
Bulle Ouvrard <bulle.ouvrard@gmail.com>
2024-03-28 17:37:29 +0100

    feat: update flex stories

--- src/layouts/Flex/Flex.stories.tsx
+++ src/layouts/Flex/Flex.stories.tsx
@@ -1,12 +1,17 @@
+import { useMemo } from "react";
 import styled from "styled-components";
 
 import { FlexLayout } from "./Flex.layout";
+
+import { ComponentVariants } from "@docs/blocks";
+
+import { FlexAlignment, FlexDirection, FlexGap, FlexLayoutProps } from ".";
 
 import type { Meta, StoryObj } from "@storybook/react";
 
 const meta = {
   component: FlexLayout,
-  title: "Layouts/Flex",
+  title: "Layouts/Flex/Stories",
 } satisfies Meta<typeof FlexLayout>;
 
 export default meta;
@@ -20,8 +25,8 @@ const FlexItem = styled.div`
   box-shadow: var(--shadow-button);
 `;
 
-const flexItems = Array.from({ length: 5 }, (_, index) => (
-  <FlexItem key={index} />
+const flexItems = Array.from({ length: 3 }, (_, index) => (
+  <FlexItem key={index} className="flex-item-story" />
 ));
 
 export const Default: Story = {
@@ -32,19 +37,83 @@ export const Default: Story = {
   },
 };
 
-export const Row: Story = {
-  args: {
-    children: flexItems,
-    gap: "s-8",
-    justify: "space-between",
-    wrap: true,
-  },
+export const Direction = (props: Partial<FlexLayoutProps>) => {
+  const directions: FlexDirection[] = ["row", "column"];
+  const defaultProps = useMemo(() => {
+    return {
+      ...FlexLayout.defaultProps,
+      ...props,
+    };
+  }, [props]);
+
+  return (
+    <ComponentVariants
+      defaults={defaultProps}
+      variants={directions}
+      for="direction"
+      of={FlexLayout}
+      scaling={1.1}
+      direction="column"
+      align="center"
+    />
+  );
+};
+Direction.args = {
+  children: flexItems,
+  gap: "s-4",
+  justify: "center",
+  align: "center",
 };
 
-export const Column: Story = {
-  args: {
-    children: flexItems,
-    gap: "s-4",
-    direction: "column",
-  },
+export const Gap = (props: Partial<FlexLayoutProps>) => {
+  const gaps: FlexGap[] = ["s-1", "24px", "3rem", 5];
+  const defaultProps = useMemo(() => {
+    return {
+      ...FlexLayout.defaultProps,
+      ...props,
+    };
+  }, [props]);
+
+  return (
+    <ComponentVariants
+      defaults={defaultProps}
+      variants={gaps}
+      for="gap"
+      of={FlexLayout}
+      scaling={1.1}
+      direction="column"
+      align="center"
+    />
+  );
 };
+Gap.args = { children: flexItems, justify: "center" };
+
+export const Justify = (props: Partial<FlexLayoutProps>) => {
+  const justify: FlexAlignment[] = [
+    "start",
+    "center",
+    "space-between",
+    "end",
+    "space-evenly",
+  ];
+  const defaultProps = useMemo(() => {
+    return {
+      ...FlexLayout.defaultProps,
+      ...props,
+    };
+  }, [props]);
+
+  return (
+    <ComponentVariants
+      defaults={defaultProps}
+      variants={justify}
+      for="justify"
+      of={FlexLayout}
+      scaling={1.1}
+      direction="column"
+      align="center"
+      fill="row"
+    />
+  );
+};
+Justify.args = { children: flexItems, gap: "s-1" };
