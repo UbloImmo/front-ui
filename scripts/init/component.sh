dd70bac8ce8b5cbb4370438b96b52ad79ef7009e
blksnk <art.jnveigel@gmail.com>
2024-05-13 16:18:30 +0200

    fix: component init script typo

--- scripts/init/component.sh
+++ scripts/init/component.sh
@@ -34,7 +34,7 @@ echo "creating new component $component_name"
 mkdir_if_missing "$component_path"
 mkdir_if_missing "$component_path/docs"
 
-touch_if_missing "$component_paht/$component_name.component.tsx"
+touch_if_missing "$component_path/$component_name.component.tsx"
 touch_if_missing "$component_path/$component_name.styles.ts"
 touch_if_missing "$component_path/$component_name.stories.tsx"
 touch_if_missing "$component_path/$component_name.test.ts"
