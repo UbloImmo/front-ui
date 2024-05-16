8ec850b00d4c51976aeccae8286c051313087563
Bulle Ouvrard <bulle.ouvrard@gmail.com>
2024-05-16 15:51:21 +0200

    feat: removed unused util phone function, added JSdoc

--- src/components/Input/PhoneInput/PhoneInput.utils.tsx
+++ src/components/Input/PhoneInput/PhoneInput.utils.tsx
@@ -1,13 +1,13 @@
-import { CountryData, defaultCountries } from "react-international-phone";
-
-export const countryList: CountryData[] = defaultCountries.map(
-  ([name, ...country]) => [name, ...country]
-);
-
+/**
+ * Converts a phone number to French format by replacing '0' with '+33 ' if it starts with '0'.
+ *
+ * @param {string} base - The input phone number.
+ * @return {string} the formatted phone number.
+ */
 export const defaultToFrenchPhone = (base: string) => {
   base = base.trim();
   if (base.startsWith("0")) {
-    return base.replace("0", "+33");
+    return base.replace("0", "+33 ");
   }
   return base;
 };
