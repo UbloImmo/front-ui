193bd1188fdde6018f0a27a78042eea1748c97fa
Bulle Ouvrard <bulle.ouvrard@gmail.com>
2024-05-16 15:52:14 +0200

    feat: full coverage test on PhoneInput component

--- src/components/Input/PhoneInput/PhoneInput.test.ts
+++ src/components/Input/PhoneInput/PhoneInput.test.ts
@@ -1,7 +1,8 @@
 import { Nullable } from "@ubloimmo/front-util";
-import { describe, expect, mock } from "bun:test";
+import { describe, expect, it, mock } from "bun:test";
 
 import { PhoneInput } from "./PhoneInput.component";
+import { defaultToFrenchPhone } from "./PhoneInput.utils";
 import { InputProps } from "../Input.types";
 
 import { testComponentFactory } from "@/tests";
@@ -29,7 +30,7 @@ describe("Input", () => {
     value: "+33612345678",
   })("should hold a given value", ({ queryByTestId }) => {
     const input = queryByTestId(testId) as HTMLInputElement;
-    expect(input).toBeDefined();
+    expect(input).not.toBeNull();
     expect(input.value).toBe("+33 6 12 34 56 78");
   });
 
@@ -40,6 +41,7 @@ describe("Input", () => {
   })(
     "should trigger onChange",
     async ({ queryByTestId }, { click, keyboard }) => {
+      onChange.mockReset();
       const input = queryByTestId(testId) as HTMLInputElement;
       await click(input);
       await keyboard("+33612345678");
@@ -47,17 +49,65 @@ describe("Input", () => {
     }
   );
 
-  // testPhoneInput({
-  //   onChange,
-  //   value: "",
-  // })(
-  //   "should update phone value correctly",
-  //   async ({ queryByTestId }, { click, keyboard }) => {
-  //     const input = queryByTestId(testId) as HTMLInputElement;
-  //     await click(input);
-  //     // adding deleting behavior
-  //     await keyboard("0");
-  //     expect(input.value).toBe("+33 ");
-  //   }
-  // );
+  testPhoneInput({
+    onChange,
+    value: "",
+  })(
+    "should update phone value correctly",
+    async ({ queryByTestId }, { click, keyboard }) => {
+      onChange.mockReset();
+      const input = queryByTestId(testId) as HTMLInputElement;
+      await click(input);
+      await keyboard("[Backspace][Backspace][Backspace][Backspace]");
+      await keyboard("0");
+      expect(input.value).toBe("+33 ");
+    }
+  );
+
+  testPhoneInput({
+    onChange,
+  })(
+    "should trigger country control on click",
+    async ({ queryByTestId, queryByRole }, { click }) => {
+      onChange.mockReset();
+      const input = queryByTestId(testId) as Nullable<HTMLInputElement>;
+      expect(input).not.toBeNull();
+      const container = queryByTestId("input-phone-container");
+      expect(container).not.toBeNull();
+      const button = queryByRole("combobox");
+      expect(button).not.toBeNull();
+
+      await click(button as HTMLButtonElement);
+      const countryOption = container?.querySelector(
+        `li[role="option"][data-country="ie"]`
+      );
+      expect(countryOption).not.toBeNull();
+      expect(countryOption?.textContent).toBe("Ireland+353");
+      await click(countryOption as HTMLLIElement);
+      expect(input?.value).toBe("+353 ");
+
+      expect(onChange).toHaveBeenCalledWith("+353");
+    }
+  );
+});
+
+describe("Input", () => {
+  describe("PhoneInput", () => {
+    describe("defaultToFrenchPhone", () => {
+      it("should default to french phone", () => {
+        expect(defaultToFrenchPhone("06 12 34 56 78")).toBe(
+          "+33 6 12 34 56 78"
+        );
+      });
+
+      it("should not change valid phone", () => {
+        expect(defaultToFrenchPhone("+33 6 12 34 56 78")).toBe(
+          "+33 6 12 34 56 78"
+        );
+        expect(defaultToFrenchPhone("+49 6 12 34 56 78")).toBe(
+          "+49 6 12 34 56 78"
+        );
+      });
+    });
+  });
 });
