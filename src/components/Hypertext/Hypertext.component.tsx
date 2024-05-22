39bbf847950dbb4717a4ad1f5cafd3435f9551a1
Bulle Ouvrard <bulle.ouvrard@gmail.com>
2024-05-22 11:24:08 +0200

    feat: reviewed color on hover

--- src/components/Hypertext/Hypertext.component.tsx
+++ src/components/Hypertext/Hypertext.component.tsx
@@ -1,14 +1,17 @@
+import { useState } from "react";
 import styled from "styled-components";
 
 import { hypertextStyle } from "./Hypertext.styles";
-import { HypertextProps } from "./Hypertext.types";
+import { DefaultHypertextProps, HypertextProps } from "./Hypertext.types";
 import { Icon } from "../Icon";
 import { Text } from "../Text";
 
+import { PaletteColor } from "@types";
 import { isEmptyString, useLogger, useMergedProps } from "@utils";
 
-const defaultHypertextProps: HypertextProps = {
+const defaultHypertextProps: DefaultHypertextProps = {
   children: "[Hypertext]",
+  title: "",
   href: "",
 };
 
@@ -24,25 +27,38 @@ const Hypertext = (props: HypertextProps): JSX.Element => {
   const { warn } = useLogger("Hypertext");
 
   const mergedProps = useMergedProps(defaultHypertextProps, props);
-  const { children, href } = mergedProps;
+  const { children, href, title } = mergedProps;
+
+  const [isHovering, setIsHovering] = useState(false);
 
   if (isEmptyString(href)) {
+    warn(`Missing required href, please provide a redirection link`);
+  }
+
+  if (isEmptyString(title)) {
     warn(
-      `Missing required href, please provide a redirection link to the component`
+      `Missing required title, please provide a title for better accessibility`
     );
   }
 
+  const hypertextColor: PaletteColor = isHovering
+    ? "primary-dark"
+    : "primary-base";
+
   return (
     <HypertextContainer
+      title={title}
       href={href}
+      onMouseEnter={() => setIsHovering(true)}
+      onMouseLeave={() => setIsHovering(false)}
       target="_blank"
       rel="noreferrer"
       data-testid="hypertext"
     >
-      <Text size="s" weight="medium">
+      <Text size="m" weight="medium" color={hypertextColor}>
         {children}
       </Text>
-      <Icon name="BoxArrowUpRight" size="s-3" />
+      <Icon name="BoxArrowUpRight" size="s-3" color={hypertextColor} />
     </HypertextContainer>
   );
 };
--- src/components/Hypertext/Hypertext.styles.tsx
+++ src/components/Hypertext/Hypertext.styles.tsx
@@ -3,7 +3,6 @@ import { css } from "styled-components";
 export const hypertextStyle = css`
   font-size: inherit;
   font-weight: inherit;
-  color: var(--primary-base);
   display: inline-flex;
   align-items: center;
   gap: var(--s-1);
@@ -13,24 +12,9 @@ export const hypertextStyle = css`
   white-space: nowrap;
   cursor: pointer;
 
-  & > span {
-    color: var(--primary-base);
-    transition: color 300ms ease-out 0s;
-  }
-
+  & > span,
   & > svg,
   & > svg > path {
-    transition: fill 300ms ease-out 0s;
-  }
-
-  &:hover {
-    span {
-      color: var(--primary-dark);
-    }
-
-    svg,
-    svg > path {
-      fill: var(--primary-dark);
-    }
+    transition-duration: 150ms;
   }
 `;
