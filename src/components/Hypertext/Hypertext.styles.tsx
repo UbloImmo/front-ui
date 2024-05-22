f383ace9dbd07d216d554793ee91f0d75705cc62
Bulle Ouvrard <bulle.ouvrard@gmail.com>
2024-05-22 13:23:49 +0200

    feat: improve transitions and display styles

--- src/components/Hypertext/Hypertext.styles.tsx
+++ src/components/Hypertext/Hypertext.styles.tsx
@@ -1,20 +1,51 @@
 import { css } from "styled-components";
 
 export const hypertextStyle = css`
-  font-size: inherit;
-  font-weight: inherit;
-  display: inline-flex;
-  align-items: center;
-  gap: var(--s-1);
-  text-decoration: none;
+  display: inline;
   width: min-content;
   min-width: min-content;
-  white-space: nowrap;
   cursor: pointer;
+
+  &,
+  & > span[data-testid="text"] {
+    text-decoration: none;
+    font-size: inherit;
+    font-weight: inherit;
+    line-height: inherit;
+    letter-spacing: inherit;
+  }
+
+  & > span[data-testid="text"] {
+    display: inherit;
+  }
 
   & > span,
   & > svg,
   & > svg > path {
-    transition-duration: 150ms;
+    transition-duration: 300ms;
+    transition-property: color, fill;
+    transition-timing-function: ease-out;
+  }
+
+  & > svg[data-testid="icon"]:last-child {
+    margin-left: var(--s-1);
+    display: inline-block;
+  }
+
+  &:hover {
+    & > span[data-testid="text"],
+    & > svg[data-testid="icon"]:last-child,
+    & > svg[data-testid="icon"]:last-child > path {
+      transition-duration: 150ms;
+    }
+
+    & > span[data-testid="text"] {
+      color: var(--primary-dark);
+    }
+
+    & > svg[data-testid="icon"]:last-child,
+    & > svg[data-testid="icon"]:last-child > path {
+      fill: var(--primary-dark);
+    }
   }
 `;
