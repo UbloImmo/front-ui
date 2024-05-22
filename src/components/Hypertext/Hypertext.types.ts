add8da63abe0e4f8ff33ef60f7e3f046ffc33236
Bulle Ouvrard <bulle.ouvrard@gmail.com>
2024-05-22 13:24:31 +0200

    fix: removed unnecessary @default on props

--- src/components/Hypertext/Hypertext.types.ts
+++ src/components/Hypertext/Hypertext.types.ts
@@ -5,21 +5,18 @@ export type HypertextProps = {
    * The Hypertext's rendered text.
    * @type {ReactNode}
    * @required
-   * @default null
    */
   children: ReactNode;
   /**
    * The Hypertext's destination URL.
    * @type {string}
    * @required
-   * @default ""
    */
   href: string;
   /**
    * The Hypertext's additional information when hovered.
    * @type {string}
    * @required
-   * @default ""
    */
   title: string;
 };
