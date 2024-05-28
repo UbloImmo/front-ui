d7f2435d61f1ac5f295a4cc43fc025b4822d79d0
blksnk <art.jnveigel@gmail.com>
2024-05-28 13:52:36 +0200

    fix: lint

--- src/themes/theme.ts
+++ src/themes/theme.ts
@@ -101,5 +101,5 @@ export const applyFavicon = (
   }
   // check
   if (link16?.href === x16 || link32?.href === x32) return true;
-  return (flink16?.href === x16 || link32?.href === x32)
+  return link16?.href === x16 || link32?.href === x32;
 };
