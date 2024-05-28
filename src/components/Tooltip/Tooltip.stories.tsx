2a8565a635d4ea41f74d3e419a9aa700f32d0850
Bulle Ouvrard <bulle.ouvrard@gmail.com>
2024-05-28 16:26:36 +0200

    feat: Tooltip stories and docs

--- src/components/Tooltip/Tooltip.stories.tsx
+++ src/components/Tooltip/Tooltip.stories.tsx
@@ -1,14 +1,20 @@
 import { Meta, StoryObj } from "@storybook/react";
+import { Nullable, objectEntries } from "@ubloimmo/front-util";
 import styled from "styled-components";
 
 import { Tooltip } from "./Tooltip.component";
+import { TooltipDirection, TooltipProps } from "./Tooltip.types";
+import { Badge } from "../Badge";
+import { Text } from "../Text";
+
+import { useMergedProps } from "@utils";
 
 const Container = styled.div`
-  height: 50vh;
-  width: 50vw;
+  height: 6rem;
+  width: 100%;
   display: flex;
   align-items: center;
-  justify-content: center;
+  justify-content: space-evenly;
 `;
 
 const meta: Meta<typeof Tooltip> = {
@@ -19,20 +25,54 @@ const meta: Meta<typeof Tooltip> = {
       options: ["top", "bottom", "left", "right"],
     },
   },
-  decorators: [
-    (Story) => (
-      <Container>
-        <Story />
-      </Container>
-    ),
-  ],
 } satisfies Meta<typeof Tooltip>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {
-  args: {
-    children: null,
-  },
+export const Default: Story = {};
+
+const directions: TooltipDirection[] = ["top", "bottom", "left", "right"];
+
+export const Directions = (props: Partial<TooltipProps>) => {
+  const defaultProps = useMergedProps(Tooltip.defaultProps, props);
+  return (
+    <Container>
+      {directions.map((direction) => (
+        <Tooltip {...defaultProps} direction={direction} key={direction} />
+      ))}
+    </Container>
+  );
+};
+
+const contents = ["Some hint", "A much longer and much more descriptive hint"];
+
+export const Content = (props: Partial<TooltipProps>) => {
+  const defaultProps = useMergedProps(Tooltip.defaultProps, props);
+  return (
+    <Container>
+      {contents.map((content) => (
+        <Tooltip {...defaultProps} content={content} key={content} />
+      ))}
+    </Container>
+  );
+};
+
+const children: Record<string, Nullable<JSX.Element>> = {
+  null: null,
+  badge: <Badge />,
+  text: <Text>Text</Text>,
+};
+
+export const Children = (props: Partial<TooltipProps>) => {
+  const defaultProps = useMergedProps(Tooltip.defaultProps, props);
+  return (
+    <Container>
+      {objectEntries(children).map(([key, child]) => (
+        <Tooltip {...defaultProps} key={key}>
+          {child}
+        </Tooltip>
+      ))}
+    </Container>
+  );
 };
