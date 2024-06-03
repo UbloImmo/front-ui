e64b74da7993a74672f8b001d48a5a72d7667a6c
blksnk <art.jnveigel@gmail.com>
2024-06-03 15:23:37 +0200

    feat: field component & docs

--- src/components/Field/Field.types.ts
+++ src/components/Field/Field.types.ts
@@ -1,5 +1,13 @@
-export type FieldProps = {
-  // TODO
-};
+import type { InputType, GenericInputProps } from "../Input";
+import type { InputAssistiveTextProps } from "../InputAssistiveText";
+import type { InputLabelProps } from "../InputLabel";
+import type { StyleOverrideProps } from "@types";
 
-export type FieldDefaultProps = Required<FieldProps>;
+export type FieldProps<TType extends InputType> = GenericInputProps<TType> &
+  InputAssistiveTextProps &
+  StyleOverrideProps &
+  Omit<InputLabelProps, "children">;
+
+export type FieldDefaultProps<TType extends InputType = InputType> = Required<
+  FieldProps<TType>
+>;
