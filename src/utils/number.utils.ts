aa269e632def179d2f56eea77560e40e7545813c
blksnk <art.jnveigel@gmail.com>
2024-06-04 18:43:54 +0200

    feat: more number utils

--- src/utils/number.utils.ts
+++ src/utils/number.utils.ts
@@ -21,3 +21,46 @@ export const lerp = (a: number, b: number, t: number): number => {
 export const clamp = (value: number, min: number, max: number): number => {
   return Math.min(Math.max(value, min), max);
 };
+
+/**
+ * Cuts fraction digits off a decimal number without rounding errors.
+ * @param {number} num - Floating point number to truncate
+ * @param {number} fractionDigits - The number of digits to appear after the decimal point.
+ * @return {number} - Truncated floating point number
+ *
+ * @remarks More precise than `Number.toFixed()`. See {@link https://arc.net/l/quote/ivxptfyt}
+ */
+export const toFixed = (num: number, fractionDigits: number): number => {
+  const reg = new RegExp("^-?\\d+(?:.\\d{0," + (fractionDigits || -1) + "})?");
+  return parseFloat((num.toString().match(reg) ?? [])[0] ?? "");
+};
+
+/**
+ * Determines if a number is negative.
+ *
+ * @param {number} num - The number to check.
+ * @return {boolean} Returns true if the number is negative, false otherwise.
+ */
+export const isNegative = (num: number): boolean => {
+  return num < 0;
+};
+
+/**
+ * Checks if a number is positive.
+ *
+ * @param {number} num - The number to check.
+ * @return {boolean} Returns true if the number is positive, false otherwise.
+ */
+export const isPositive = (num: number): boolean => {
+  return num > 0;
+};
+
+/**
+ * Checks if a number is zero.
+ *
+ * @param {number} num - The number to check.
+ * @return {boolean} Returns true if the number is zero, false otherwise.
+ */
+export const isZero = (num: number): boolean => {
+  return num === 0;
+};
