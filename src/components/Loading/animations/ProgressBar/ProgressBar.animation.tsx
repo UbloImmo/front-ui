50c594428f1198be8ba5d1665e2cb3e34e67453b
Bulle Ouvrard <bulle.ouvrard@gmail.com>
2024-06-18 16:25:32 +0200

    feat: added ProgressBar animation type

---
+++ src/components/Loading/animations/ProgressBar/ProgressBar.animation.tsx
@@ -0,0 +1,32 @@
+import styled from "styled-components";
+
+import { ProgressBarStyle } from "./ProgressBar.styles";
+
+import { useClassName, useStyleProps } from "@utils";
+
+import type { LoadingAnimationProps } from "../../Loading.types";
+import type { StyleProps } from "@types";
+
+/**
+ * Renders a ProgressBar loading animation
+ *
+ * @param {LoadingAnimationProps} props - the loading animation props.
+ * @return {JSX.Element} the rendered spinner component
+ */
+export const ProgressBar = (props: LoadingAnimationProps): JSX.Element => {
+  const styleProps = useStyleProps(props);
+
+  const className = useClassName(props);
+
+  return (
+    <Renderer
+      {...styleProps}
+      data-testid={props.testId}
+      className={className}
+    />
+  );
+};
+
+const Renderer = styled.div<StyleProps<LoadingAnimationProps>>`
+  ${ProgressBarStyle}
+`;
---
+++ src/components/Loading/animations/ProgressBar/ProgressBar.styles.ts
@@ -0,0 +1,62 @@
+import { StyleFunction, css, keyframes } from "styled-components";
+
+import { LoadingAnimationProps } from "../../Loading.types";
+
+import { parseFixedLength } from "@/sizes/size.utils";
+import { StyleProps } from "@types";
+import { cssRem, cssVarUsage, extractRem } from "@utils";
+
+const cssAnim = keyframes`
+  0% {
+    left: 0;
+    transform: translateX(-100%);
+  }
+  15% {
+    width: 10%;
+  }
+  50% {
+    width: 60%;
+  }
+  100% {
+    width: 10%;
+    left: 100%;
+    transform: translateX(0%);
+  }
+`;
+
+export const ProgressBarStyle: StyleFunction<
+  StyleProps<LoadingAnimationProps>
+> = ({ $size, $color }) => {
+  const size = parseFixedLength($size);
+  const sizeRem = extractRem(size);
+  return css`
+    width: ${size};
+    min-width: ${cssRem(Math.max(sizeRem, 1))};
+    border-radius: var(--s-2);
+    height: var(--s-1);
+    position: relative;
+    background: white;
+    overflow: hidden;
+    cursor: progress;
+
+    &:before {
+      content: "";
+      position: absolute;
+      inset: 0;
+      background: ${`var(--${$color}-20)`};
+    }
+
+    &:after {
+      content: "";
+      height: var(--s-1);
+      background: ${cssVarUsage($color)};
+      border-radius: var(--s-2);
+      position: absolute;
+      top: 0;
+      left: 0;
+      box-sizing: border-box;
+      animation: ${cssAnim} 1400ms cubic-bezier(0.6, 0, 0.22, 1) infinite
+        running;
+    }
+  `;
+};
