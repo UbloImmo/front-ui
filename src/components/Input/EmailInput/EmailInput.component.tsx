55d3d7c19cc5954cefeeeadd28621353623775cf
Bulle Ouvrard <bulle.ouvrard@gmail.com>
2024-07-02 10:41:05 +0200

    fix: update component version

--- src/components/Input/EmailInput/EmailInput.component.tsx
+++ src/components/Input/EmailInput/EmailInput.component.tsx
@@ -31,7 +31,7 @@ const defaultEmailInputProps: DefaultInputProps<"email"> = {
 /**
  * Renders an email input component. Does some rudimentary format validation on the input value.
  *
- * @version 0.0.3
+ * @version 0.0.4
  * @param {InputProps<"email">} props - The input props.
  * @return {JSX.Element} The rendered text input component.
  */
