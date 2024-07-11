5bf1c04b523abdea0db9260b84350645294c0a1b
Bulle Ouvrard <bulle.ouvrard@gmail.com>
2024-07-11 14:04:45 +0200

    fix: refacto and data-active handle of active state

--- src/components/Switch/Switch.styles.ts
+++ src/components/Switch/Switch.styles.ts
@@ -1,80 +1,73 @@
-import styled, { css, keyframes, RuleSet } from "styled-components";
+import { css, keyframes, RuleSet } from "styled-components";
 
-import { cssVarUsage } from "@utils";
-
-import type { SwitchStyleProps } from "./Switch.types";
-
-export const SwitchContainer = styled.div`
-  display: flex;
-  align-items: center;
-  gap: var(--s-2);
-  cursor: default;
-`;
-
-export const SwitchToggleStyles = ({
-  $active,
-  $disabled,
-}: SwitchStyleProps): RuleSet => {
+export const SwitchContainerStyles = (): RuleSet => {
   return css`
     cursor: pointer;
     width: var(--s-10);
     height: 1.375rem;
     border-radius: var(--s-5);
-    padding: 0.2rem;
-    background-color: ${$active
-      ? $disabled
-        ? cssVarUsage("gray-600")
-        : cssVarUsage("primary-base")
-      : cssVarUsage("gray-300")};
+    padding: 0.15rem 0.2rem;
+    border: none;
+    background-color: var(--gray-300);
     transition: background-color 0.4s ease;
+
+    &[data-active="true"] {
+      background-color: var(--primary-base);
+
+      &:disabled {
+        background-color: var(--gray-600);
+      }
+    }
+
+    &[data-active="false"]:not(:disabled) {
+      background-color: var(--gray-300);
+    }
   `;
 };
 
 const switchActiveCSSAnim = keyframes`
-  0% { 
-    transform: translateX(0) scale(1);
+  from {
+    transform: translateX(0);
   }
   25% {
-    transform: scale(0.8);
+    transform: scaleY(0.8);
     width: var(--s-6);
   }
-  100% { 
-    width: var(--s-4);
+  to {
     transform: translateX(1.1rem);
-    
   }
 `;
 
 const switchInactiveCSSAnim = keyframes`
-  0% { 
-    transform: translateX(1.1rem) scale(1);
+  from {
+    transform: translateX(1.1rem);
   }
   25% {
-    transform: scale(0.8);
+    transform: scaleY(0.8);
     width: var(--s-6);
   }
-  100% { 
-    width: var(--s-4);
+  to {
     transform: translateX(0);
   }
 `;
 
-export const SwitchToggleCheckboxStyles = ({
-  $active,
-}: SwitchStyleProps): RuleSet => {
+export const SwitchHandleStyles = (): RuleSet => {
   return css`
     width: var(--s-4);
     height: var(--s-4);
     border-radius: var(--s-4);
     background: white;
     cursor: inherit;
-    transform: ${$active ? "translateX(1.1rem)" : "translateX(0)"};
-    ${$active
-      ? css`
-          animation: ${switchActiveCSSAnim} 250ms ease-out;
-        `
-      : css`
-          animation: ${switchInactiveCSSAnim} 300ms ease-out;
-        `}
+    transition: transform 300ms ease-out;
+
+    &[data-active="true"] {
+      animation: ${switchActiveCSSAnim} 300ms ease-out;
+      transform: translateX(1.1rem);
+    }
+
+    &[data-active="false"] {
+      animation: ${switchInactiveCSSAnim} 300ms ease-out;
+      transform: translateX(0);
+    }
   `;
 };
