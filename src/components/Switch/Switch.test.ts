587dc3dbebd99d6d9d1518b740292f03414d57f2
Bulle Ouvrard <bulle.ouvrard@gmail.com>
2024-07-16 11:03:25 +0200

    feat: add onChange prop tests

--- src/components/Switch/Switch.test.ts
+++ src/components/Switch/Switch.test.ts
@@ -1,4 +1,4 @@
-import { expect } from "bun:test";
+import { expect, mock } from "bun:test";
 
 import { Switch } from "./Switch.component";
 
@@ -10,8 +10,8 @@ testSwitch(Switch.defaultProps)("should render", ({ queryByTestId }) => {
   expect(queryByTestId("switch")).not.toBeNull();
 });
 
-testSwitch({ ...Switch.defaultProps })(
-  "should toggle active state on click",
+testSwitch({ ...Switch.defaultProps, active: false })(
+  "should toggle active state to true on click",
   async ({ queryByTestId }, { click }) => {
     const switchContainer = (await queryByTestId(
       "switch-container"
@@ -19,6 +19,18 @@ testSwitch({ ...Switch.defaultProps })(
     expect(switchContainer).not.toBeNull();
     await click(switchContainer);
     expect(switchContainer.getAttribute("aria-checked")).toBe("true");
+  }
+);
+
+testSwitch({ ...Switch.defaultProps, active: true })(
+  "should toggle active state to false on click",
+  async ({ queryByTestId }, { click }) => {
+    const switchContainer = (await queryByTestId(
+      "switch-container"
+    )) as HTMLButtonElement;
+    expect(switchContainer).not.toBeNull();
+    await click(switchContainer);
+    expect(switchContainer.getAttribute("aria-checked")).toBe("false");
   }
 );
 
@@ -45,5 +57,35 @@ testSwitch({ ...Switch.defaultProps, disabled: true, active: true })(
     await click(switchContainer);
     expect(switchContainer.getAttribute("aria-disabled")).toBe("true");
     expect(switchContainer.disabled).toBe(true);
+  }
+);
+
+const onChange = mock(() => {});
+
+testSwitch({ ...Switch.defaultProps, onChange })(
+  "should trigger onChange and pass the active state as argument",
+  async ({ queryByTestId }, { click }) => {
+    const switchContainer = (await queryByTestId(
+      "switch-container"
+    )) as HTMLButtonElement;
+    expect(switchContainer).not.toBeNull();
+    await click(switchContainer);
+    expect(onChange).toHaveBeenCalled();
+    expect(onChange).toHaveBeenCalledWith(true);
+    onChange.mockReset();
+  }
+);
+
+testSwitch({ ...Switch.defaultProps, active: true, onChange })(
+  "should trigger onChange and pass the inactive state as argument",
+  async ({ queryByTestId }, { click }) => {
+    const switchContainer = (await queryByTestId(
+      "switch-container"
+    )) as HTMLButtonElement;
+    expect(switchContainer).not.toBeNull();
+    await click(switchContainer);
+    expect(onChange).toHaveBeenCalled();
+    expect(onChange).toHaveBeenCalledWith(false);
+    onChange.mockReset();
   }
 );
