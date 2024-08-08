6641e4ff6915f963340ba0f286dfdbb1486cc0d2
Hamid <codin.hamster@gmail.com>
2024-08-08 16:56:22 +0200

    feat(dev-4284) - EnergyLabel Component

---
+++ src/components/EnergyLabel/EnergyLabel.component.tsx
@@ -0,0 +1,48 @@
+import { useLogger, useTestId, useMergedProps, useStyleProps } from "@utils";
+
+import type {
+  EnergyLabelProps,
+  EnergyLabelDefaultProps,
+} from "./EnergyLabel.types";
+import { StyleProps, type TestIdProps } from "@types";
+import styled from "styled-components";
+import { energyLabelStyle } from "./EnergyLabel.styles";
+import { Text } from "../Text";
+
+const defaultEnergyLabelProps: EnergyLabelDefaultProps = {
+  type: "DPE",
+  value: null,
+  state: "inactive",
+};
+
+/**
+ * Renders a EnergyLabel component, with required Value, Type and State.
+ *
+ * @version 0.0.1
+ *
+ * @param {EnergyLabelProps & TestIdProps} props - EnergyLabel component props
+ * @returns {JSX.Element}
+ */
+const EnergyLabel = (props: EnergyLabelProps & TestIdProps): JSX.Element => {
+  const { log } = useLogger("EnergyLabel");
+  const mergedProps = useMergedProps(defaultEnergyLabelProps, props);
+  const styledProps = useStyleProps(mergedProps);
+  const testId = useTestId("energy-label", props);
+
+  log(mergedProps);
+
+  return (
+    <EnergyLabelContainer data-testid={testId} {...styledProps} role="status">
+      <Text weight="bold" testId="energy-label-value" overrideTestId>
+        {mergedProps.value || "-"}
+      </Text>
+    </EnergyLabelContainer>
+  );
+};
+
+EnergyLabel.defaultProps = defaultEnergyLabelProps;
+export { EnergyLabel };
+
+const EnergyLabelContainer = styled.div<StyleProps<EnergyLabelProps>>`
+  ${energyLabelStyle}
+`;
---
+++ src/components/EnergyLabel/EnergyLabel.stories.tsx
@@ -0,0 +1,64 @@
+import { EnergyLabel } from "./EnergyLabel.component";
+
+import { componentSourceFactory } from "@docs/docs.utils";
+
+import type { EnergyLabelProps } from "./EnergyLabel.types";
+import type { Meta, StoryObj } from "@storybook/react";
+import { ComponentVariants } from "@docs/blocks";
+
+const args = {
+  ...EnergyLabel.defaultProps,
+};
+
+const componentSource = componentSourceFactory<EnergyLabelProps>(
+  "EnergyLabel",
+  {
+    value: "A",
+    type: "DPE",
+    state: "active",
+  },
+  EnergyLabel.defaultProps
+);
+
+const meta = {
+  argTypes: {
+    value: {
+      options: ["A", "B", "C", "D", "E", "F", "G"],
+    },
+  },
+  component: EnergyLabel,
+  title: "Components/EnergyLabel/Stories",
+  args: {
+    type: "DPE",
+    state: "inactive",
+  },
+  parameters: {
+    docs: componentSource(),
+  },
+} satisfies Meta<typeof EnergyLabel>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args,
+};
+
+export const ActiveState = (props: EnergyLabelProps) => {
+  return (
+    <ComponentVariants
+      defaults={props}
+      variants={["active", "inactive"]}
+      for="state"
+      of={EnergyLabel}
+      align="center"
+      propLabels
+    />
+  );
+};
+// <EnergyLabel {...args} state="active" />
+
+ActiveState.args = {
+  state: "active",
+  value: "A",
+};
