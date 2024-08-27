77756cd260cb2feb587b3dbc50e9743ec51e98cd
Bulle Ouvrard <bulle.ouvrard@gmail.com>
2024-08-27 10:23:50 +0200

    fix: type import

--- src/components/Input/IconPickerInput/IconPickerInput.types.ts
+++ src/components/Input/IconPickerInput/IconPickerInput.types.ts
@@ -1,6 +1,5 @@
-import { IconPickerProps } from "@/components/IconPicker";
-
 import type { InputProps } from "../Input.types";
+import type { IconPickerProps } from "@/components/IconPicker";
 import type { NonNullish } from "@ubloimmo/front-util";
 
 export type IconPickerInputProps = IconPickerProps & InputProps<"icon-picker">;
