2fa8b0ab580ada2e54ad3f81d5c90b73aeb9e638
Bulle Ouvrard <bulle.ouvrard@gmail.com>
2024-09-02 16:12:04 +0200

    feat: completed stories and documentation

--- src/components/Collapsible/Collapsible.stories.tsx
+++ src/components/Collapsible/Collapsible.stories.tsx
@@ -1,7 +1,9 @@
 import { Collapsible } from "./Collapsible.component";
+import { Badge } from "../Badge";
 import { StaticIcon } from "../StaticIcon";
 import { Text } from "../Text";
 
+import { ComponentVariants } from "@docs/blocks";
 import { componentSourceFactory } from "@docs/docs.utils";
 import { FlexRowLayout } from "@layouts";
 
@@ -16,47 +18,58 @@ const componentSource = componentSourceFactory<CollapsibleProps>(
   Collapsible.defaultProps
 );
 
-const defaultChildrenProperty = <Text>[children]</Text>;
+const defaultChildren = <Text>[children]</Text>;
 
-const defaultCollapsibleComponent = {
-  ...Collapsible.defaultProps,
-  children: defaultChildrenProperty,
-};
-
-const defaultCollapsibleChildren: CollapsibleDefaultProps = {
-  ...defaultCollapsibleComponent,
-  subCollapsibles: [
-    {
-      ...Collapsible.defaultProps,
-      children: defaultChildrenProperty,
-      subCollapsibles: [defaultCollapsibleComponent],
-    },
-  ],
-};
-
-const CustomCollapsibleChildren = (
+const customChildren = (
   <FlexRowLayout align="center" fill gap="s-2">
     <StaticIcon name="BuildingBlocks" color="primary" />
     <Text>Collapsible children</Text>
   </FlexRowLayout>
 );
 
+const customChildren2 = (
+  <FlexRowLayout align="center" fill gap="s-2" justify="space-between">
+    <Text>Collapsible children 2</Text>
+    <Badge label="Badge" />
+  </FlexRowLayout>
+);
+
+const defaultCollapsibleComponent = {
+  ...Collapsible.defaultProps,
+  children: defaultChildren,
+};
+
+const defaultCollapsibleChildren: CollapsibleDefaultProps = {
+  ...defaultCollapsibleComponent,
+  subCollapsibles: [
+    {
+      ...defaultCollapsibleComponent,
+      subCollapsibles: [defaultCollapsibleComponent],
+    },
+  ],
+};
+
 const CustomSubCollapsibles: CollapsibleDefaultProps[] = [
   {
     isOpen: false,
-    onOpenChange: false,
+    onOpenChange: null,
     disabled: false,
     compact: false,
-    children: CustomCollapsibleChildren,
+    children: customChildren2,
     subCollapsibles: [defaultCollapsibleComponent, defaultCollapsibleComponent],
   },
   {
     isOpen: false,
-    onOpenChange: false,
+    onOpenChange: null,
     disabled: false,
     compact: false,
-    children: <Text>Sub collapsible children 2</Text>,
-    subCollapsibles: null,
+    children: customChildren2,
+    subCollapsibles: [
+      {
+        ...Collapsible.defaultProps,
+        children: customChildren,
+      },
+    ],
   },
 ];
 
@@ -79,7 +92,20 @@ export const Default: Story = {};
 
 export const CustomChildren: Story = {
   args: {
-    children: CustomCollapsibleChildren,
+    children: customChildren,
     subCollapsibles: CustomSubCollapsibles,
   },
+};
+
+export const Compact = () => {
+  return (
+    <ComponentVariants
+      of={Collapsible}
+      variants={[false, true]}
+      for="compact"
+      defaults={{ ...defaultCollapsibleChildren, isOpen: true }}
+      columns={2}
+      propLabels
+    />
+  );
 };
