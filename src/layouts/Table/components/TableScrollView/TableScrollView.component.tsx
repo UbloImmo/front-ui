fb6645e34ce9076fbe1eed5671c7a4b35c97009b
Bulle Ouvrard <bulle.ouvrard@gmail.com>
2024-09-09 11:11:13 +0200

    chore: add documentation on tablescrollview component

--- src/layouts/Table/components/TableScrollView/TableScrollView.component.tsx
+++ src/layouts/Table/components/TableScrollView/TableScrollView.component.tsx
@@ -4,6 +4,19 @@ import { tableScrollViewStyles } from "./TableScrollView.styles";
 
 import type { TableProps } from "../../Table.types";
 
+/**
+ * A wrapper component designed to enable horizontal scrolling in a `Table`.
+ *
+ * @example
+ * <TableScrollView>
+ *   <Table>
+ *     {...}
+ *   </Table>
+ * </TableScrollView>
+ *
+ * @param {TableProps} props - The component props.
+ * @returns The table scroll view component.
+ */
 const TableScrollView = (props: TableProps) => {
   return <StyledTableScrollView>{props.children}</StyledTableScrollView>;
 };
