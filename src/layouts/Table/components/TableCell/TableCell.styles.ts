8641501d866a6ff01337b44408febea90eef0549
Bulle Ouvrard <bulle.ouvrard@gmail.com>
2024-09-09 11:11:47 +0200

    fix: add style to prevent text wrapping

--- src/layouts/Table/components/TableCell/TableCell.styles.ts
+++ src/layouts/Table/components/TableCell/TableCell.styles.ts
@@ -5,5 +5,6 @@ import type { TableCellStyleProps } from "../../Table.types";
 export const tableCellStyles = ({ $padded }: TableCellStyleProps) => {
   return css`
     ${$padded && `padding: var(--s-2) var(--s-4);`}
+    white-space: nowrap;
   `;
 };
