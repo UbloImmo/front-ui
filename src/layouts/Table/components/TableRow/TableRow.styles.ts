e4b16d01fa8e5b5fcbf69c48c8329268563b9f58
blksnk <art.jnveigel@gmail.com>
2024-12-03 15:29:56 +0100

    style: table row

--- src/layouts/Table/components/TableRow/TableRow.styles.ts
+++ src/layouts/Table/components/TableRow/TableRow.styles.ts
@@ -1,24 +1,45 @@
 import { css, type RuleSet } from "styled-components";
 
-export const tableRowStyles = (): RuleSet => {
+import type { TableRowStyleProps } from "./TableRow.types";
+
+const tableRowFormStyles = () => css`
+  & > td {
+    border-right: 1px solid var(--gray-200);
+    border-bottom: 1px solid var(--gray-200);
+  }
+
+  & > td:first-child {
+    border-left: 1px solid var(--gray-200);
+  }
+
+  &:first-child > td {
+    border-top: 1px solid var(--gray-200);
+  }
+
+  // remove double border
+  tfoot > &:first-child > td {
+    border-top: none;
+  }
+`;
+
+const tableRowListStyles = () => css`
+  padding: var(--s-2) 0;
+  background: white;
+
+  &:nth-child(even) {
+    background: var(--gray-50);
+  }
+
+  &:not(:last-child) {
+    box-shadow: var(--border-bottom);
+  }
+`;
+
+export const tableRowStyles = ({ $style }: TableRowStyleProps): RuleSet => {
+  const baseStyle = $style === "list" ? tableRowListStyles : tableRowFormStyles;
+
   return css`
-    & > td {
-      border-right: 1px solid var(--gray-200);
-      border-bottom: 1px solid var(--gray-200);
-    }
-
-    & > td:first-child {
-      border-left: 1px solid var(--gray-200);
-    }
-
-    &:first-child > td {
-      border-top: 1px solid var(--gray-200);
-    }
-
-    // remove double border
-    tfoot > &:first-child > td {
-      border-top: none;
-    }
+    ${baseStyle}
 
     tbody > &:first-child > td:first-child {
       border-top-left-radius: var(--s-1);
