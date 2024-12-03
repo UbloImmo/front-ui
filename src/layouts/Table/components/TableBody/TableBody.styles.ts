be63ed88da21468f0fc9f883268ce3991669996b
blksnk <art.jnveigel@gmail.com>
2024-12-03 15:29:41 +0100

    style: table body border radius

--- src/layouts/Table/components/TableBody/TableBody.styles.ts
+++ src/layouts/Table/components/TableBody/TableBody.styles.ts
@@ -1,9 +1,12 @@
 import { css, type RuleSet } from "styled-components";
+
 import type { TableBodyStyleProps } from "./TableBody.types";
 
 export const tableBodyStyles = ({ $style }: TableBodyStyleProps): RuleSet => {
   if ($style === "form") return css``;
   return css`
     border: 1px solid var(--primary-light);
+    border-radius: var(--s-1);
+    overflow: hidden;
   `;
 };
