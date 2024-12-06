7c7ebfe61d4c9c6ca6e8b3ef35713fd4bf0d3c08
Vasiliki Doropoulou <vasiliki@ublo.immo>
2024-12-06 15:36:38 +0100

    fix: replace overflow hidden with mid width for children

--- src/components/Form/components/FormTable/FormTableCell/FormTableFieldCell.component.tsx
+++ src/components/Form/components/FormTable/FormTableCell/FormTableFieldCell.component.tsx
@@ -90,9 +90,11 @@ const FormTableDisplayCellInner = styled.div`
   align-items: center;
   justify-content: flex-start;
   width: 100%;
-  overflow: hidden;
-  text-overflow: ellipsis;
-  white-space: nowrap;
+
+  > * {
+    min-width: 0;
+    width: 100%;
+  }
 
   @media only screen and (max-width: ${breakpointsPx.XS}) {
     min-height: var(--s-8);
