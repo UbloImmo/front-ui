5225cc8516096e43350ee8cffd3d16f8d6b1969f
blksnk <art.jnveigel@gmail.com>
2025-01-17 13:22:32 +0100

    fix: pre-push script

--- scripts/pre-push.sh
+++ scripts/pre-push.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
 
 # Count source files to push
-source_files_count = `
-git --no-pager diff --cached --name-only --cached
-| grep -E "^(src|docs)\/.*\.(ts|tsx|js|jsx|json)$"
-| wc -l
-`i
+source_files_count=`
+git --no-pager diff --cached --name-only --cached |
+grep -E "^(src|docs)\/.*\.(ts|tsx|js|jsx|json)$" |
+wc -l
+`
 
 if [ $source_files_count == 0 ]
 then
