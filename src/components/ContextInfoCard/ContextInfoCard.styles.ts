ef835b9320fb2b6d033de150a0ee60801029c267
Vasiliki Doropoulou <vasiliki@ublo.immo>
2025-01-20 15:55:43 +0100

    style(ui): add styled component for ContextInfoCard container

---
+++ src/components/ContextInfoCard/ContextInfoCard.styles.ts
@@ -0,0 +1,7 @@
+import { css, RuleSet } from "styled-components";
+
+export const contextInfoCardContainerStyles = (): RuleSet => css`
+  border-radius: var(--s-2);
+  border: 1px solid var(--primary-light);
+  padding: var(--s-4);
+`;
