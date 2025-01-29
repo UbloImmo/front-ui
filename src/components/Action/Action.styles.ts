cda89c06f273d61ab9cf6906da9cb3ed4467c40c
Vasiliki Doropoulou <vasiliki@ublo.immo>
2025-01-29 21:43:58 +0100

    refactor(styles): update Action component styles to support new variants

--- src/components/Action/Action.styles.ts
+++ src/components/Action/Action.styles.ts
@@ -2,22 +2,28 @@ import { css, type RuleSet } from "styled-components";
 
 import { cssVarUsage } from "@utils";
 
-import type { ActionSize, DefaultActionProps } from "./Action.types";
+import type { DefaultActionProps, ActionVariant } from "./Action.types";
 import type { CssRem, SpacingLabel, StyleProps } from "@types";
 
-const horizontalPaddings: Record<ActionSize, SpacingLabel> = {
+const horizontalPaddings: Record<ActionVariant, SpacingLabel> = {
   default: "s-2",
-  large: "s-6",
+  centered: "s-2",
+  chunky: "s-6",
+  card: "s-4",
 };
 
-const heights: Record<ActionSize, CssRem> = {
+const heights: Record<ActionVariant, CssRem> = {
   default: "2.5rem",
-  large: "5.25rem",
+  centered: "2.5rem",
+  chunky: "5.25rem",
+  card: "8rem",
 };
 
-const borderRadii: Record<ActionSize, SpacingLabel> = {
+const borderRadii: Record<ActionVariant, SpacingLabel> = {
   default: "s-1",
-  large: "s-3",
+  centered: "s-1",
+  chunky: "s-3",
+  card: "s-3",
 };
 
 export const actionTextWrappingStyles = () => css`
@@ -39,22 +45,24 @@ export const actionTextWrappingStyles = () => css`
  * @return {RuleSet} The generated CSS styles for the action container.
  */
 export const actionContainerStyles = ({
-  $size,
+  $variant,
   $disabled,
 }: StyleProps<DefaultActionProps>): RuleSet => {
-  const paddingHorizontal = horizontalPaddings[$size];
-  const height = heights[$size];
-  const borderRadius = borderRadii[$size];
+  const paddingHorizontal = horizontalPaddings[$variant];
+  const height = heights[$variant];
+  const borderRadius = borderRadii[$variant];
   const background = $disabled ? cssVarUsage("gray-50") : "#fff";
   const disabledShadow = cssVarUsage(
-    $size === "large" ? "shadow-card-elevation-low" : "shadow-button",
+    $variant === "chunky" || $variant === "card"
+      ? "shadow-card-elevation-low"
+      : "shadow-button"
   );
 
   return css`
     cursor: pointer;
     height: ${height};
     min-height: ${height};
-    max-height: ${height};
+    max-height: ${$variant === "card" ? "none" : height};
     width: 100%;
     padding: var(--s-05) var(--${paddingHorizontal});
     display: flex;
@@ -65,13 +73,11 @@ export const actionContainerStyles = ({
     box-shadow: var(--shadow-button);
     border-radius: var(--${borderRadius});
 
-    transition:
-      background 300ms ease-out 0s,
-      border-color 300ms ease-out 0s,
+    transition: background 300ms ease-out 0s, border-color 300ms ease-out 0s,
       box-shadow 300ms ease-out 0s;
 
     span[data-testid="text action-label"] {
-      ${$size === "default" &&
+      ${$variant === "default" &&
       css`
         flex: 1;
       `}
@@ -80,6 +86,38 @@ export const actionContainerStyles = ({
       max-width: fit-content;
       ${actionTextWrappingStyles}
     }
+
+    ${$variant === "centered" &&
+    css`
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      padding: var(--s-05) var(--${paddingHorizontal});
+
+      > div:first-child {
+        display: flex;
+        align-items: center;
+        gap: var(--s-2);
+        margin: 0 auto;
+      }
+
+      span[data-testid="text action-label"] {
+        white-space: nowrap;
+      }
+
+      *[data-testid="badge action-badge"] {
+        display: none;
+      }
+    `}
+
+    ${$variant === "card" &&
+    css`
+      padding: var(--s-4);
+
+      > div {
+        gap: var(--s-4);
+      }
+    `}
 
     *[data-testid="badge action-badge"] span {
       max-width: max-content;
