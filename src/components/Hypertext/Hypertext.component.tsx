b33dc0985c45e5edc852c0c6ffdc89428f3b569d
Vasiliki Doropoulou <vasiliki@ublo.immo>
2025-04-01 11:23:38 +0200

    refactor: improve Hypertext component structure

--- src/components/Hypertext/Hypertext.component.tsx
+++ src/components/Hypertext/Hypertext.component.tsx
@@ -2,10 +2,11 @@ import { MouseEventHandler, useCallback, useMemo } from "react";
 import styled from "styled-components";
 
 import { hypertextStyle } from "./Hypertext.styles";
-import { DefaultHypertextProps, HypertextProps } from "./Hypertext.types";
+import { HypertextProps } from "./Hypertext.types";
 import { Icon } from "../Icon";
 import { Text } from "../Text";
 
+import { FlexRowLayout } from "@layouts";
 import {
   isEmptyString,
   useLogger,
@@ -16,7 +17,7 @@ import {
 
 import type { PaletteColor, StyleProps } from "@types";
 
-const defaultHypertextProps: DefaultHypertextProps = {
+const defaultHypertextProps: HypertextProps = {
   children: "[Hypertext]",
   title: "",
   href: "",
@@ -40,7 +41,7 @@ const Hypertext = (props: HypertextProps): JSX.Element => {
   const { children, href, title, color, onClick } = mergedProps;
 
   // Only warn about missing href if onClick is not provided
-  if (isEmptyString(href) && !onClick) {
+  if (isEmptyString(href || "") && !onClick) {
     warn(
       `Missing required href, please provide a redirection link or an onClick handler`
     );
@@ -84,20 +85,39 @@ const Hypertext = (props: HypertextProps): JSX.Element => {
     [onClick]
   );
 
-  return (
-    <HypertextContainer
-      title={title}
-      href={href || "#"}
-      target="_blank"
-      rel="noreferrer"
-      onClick={handleClick}
-      data-testid={testId}
-      {...styleProps}
-    >
+  const content = (
+    <FlexRowLayout align="center" gap="s-1">
       <Text size="m" weight="medium" color={textColor} underline>
         {children}
       </Text>
       <Icon name="BoxArrowUpRight" size="s-3" color={iconColor} />
+    </FlexRowLayout>
+  );
+
+  if (onClick) {
+    return (
+      <HypertextButton
+        title={title}
+        onClick={handleClick}
+        data-testid={testId}
+        type="button"
+        {...styleProps}
+      >
+        {content}
+      </HypertextButton>
+    );
+  }
+
+  return (
+    <HypertextContainer
+      title={title}
+      href={href}
+      target="_blank"
+      rel="noreferrer"
+      data-testid={testId}
+      {...styleProps}
+    >
+      {content}
     </HypertextContainer>
   );
 };
@@ -106,6 +126,12 @@ Hypertext.defaultProps = defaultHypertextProps;
 
 export { Hypertext };
 
-const HypertextContainer = styled.a<StyleProps<DefaultHypertextProps>>`
+const HypertextContainer = styled.a<StyleProps<HypertextProps>>`
   ${hypertextStyle};
+`;
+
+const HypertextButton = styled.button<StyleProps<HypertextProps>>`
+  ${hypertextStyle};
+  background: none;
+  border: none;
 `;
