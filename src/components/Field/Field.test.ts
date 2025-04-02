467bb19bbd2bcd1e38476a59eb0c0df18b554a98
Vasiliki Doropoulou <vasiliki@ublo.immo>
2025-04-02 17:12:55 +0200

    test(Field): add tests for suffix prop rendering in Field component
    - Implemented tests to verify the rendering of the suffix prop in the Field component.
    - Added cases for both rendering with a suffix and without a suffix when set to null.

--- src/components/Field/Field.test.ts
+++ src/components/Field/Field.test.ts
@@ -8,6 +8,7 @@ import { testComponentFactory } from "@/tests";
 const testId = "field";
 const labelTestId = "field-label";
 const assistiveTextTestId = "field-assistive-text";
+const suffixTestId = "field-suffix";
 
 const testField = testComponentFactory("Field", Field);
 
@@ -34,3 +35,31 @@ const testEachFieldType = () => {
 };
 
 testEachFieldType();
+
+testField({
+  type: "text",
+  label: "Field with suffix",
+  suffix: "[suffix]",
+})("should render with suffix", async ({ findByTestId }) => {
+  const field = await findByTestId(testId);
+  const suffix = await findByTestId(suffixTestId);
+
+  expect(field).not.toBeNull();
+  expect(suffix).not.toBeNull();
+  expect(suffix.textContent).toBe("[suffix]");
+});
+
+testField({
+  type: "text",
+  label: "Field without suffix",
+  suffix: null,
+})(
+  "should render without suffix when suffix is null",
+  async ({ findByTestId, queryByTestId }) => {
+    const field = await findByTestId(testId);
+    const suffix = queryByTestId(suffixTestId);
+
+    expect(field).not.toBeNull();
+    expect(suffix).toBeNull();
+  }
+);
