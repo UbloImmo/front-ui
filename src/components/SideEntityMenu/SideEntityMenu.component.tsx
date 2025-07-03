c7ff7ac479c54fbadc5076ddacfe59ee798f97e2
Vasiliki Doropoulou <vasiliki@ublo.immo>
2025-07-03 16:00:06 +0200

    feat: add SideEntityMenu component for entity-based navigation

---
+++ src/components/SideEntityMenu/SideEntityMenu.component.tsx
@@ -0,0 +1,111 @@
+import { Fragment } from "react";
+import styled from "styled-components";
+
+import { SideEntityMenuItem } from "./components/SideEntityMenuItem";
+import {
+  sideEntityMenuStyles,
+  sideEntityMenuHeaderStyles,
+} from "./SideEntityMenu.styles";
+
+import {
+  useTestId,
+  useMergedProps,
+  useHtmlAttribute,
+  useClassName,
+} from "@utils";
+
+import type {
+  SideEntityMenuProps,
+  SideEntityMenuDefaultProps,
+} from "./SideEntityMenu.types";
+import type { TestIdProps } from "@types";
+
+const defaultSideEntityMenuProps: SideEntityMenuDefaultProps = {
+  menuLinks: [],
+  backLinks: [],
+  width: "15.5rem",
+  collapsedWidth: "2.75rem",
+};
+
+/**
+ * A side navigation menu component for entity-based navigation
+ *
+ * @version 0.0.1
+ *
+ * @param {SideEntityMenuProps & TestIdProps} props - SideEntityMenu component props
+ * @returns {JSX.Element}
+ */
+const SideEntityMenu = (
+  props: SideEntityMenuProps & TestIdProps
+): JSX.Element => {
+  const mergedProps = useMergedProps(defaultSideEntityMenuProps, props);
+  const testId = useTestId("side-entity-menu", props);
+  const className = useClassName(props);
+  const style = useHtmlAttribute(props.styleOverride);
+
+  const { menuLinks, backLinks, width, collapsedWidth } = mergedProps;
+
+  // Add default icon to backlinks if not provided
+  const backLinksWithIcons = backLinks.map((link) => ({
+    ...link,
+    icon: link.icon || "ArrowLeftShort",
+  }));
+
+  return (
+    <StyledSideEntityMenu
+      data-testid={testId}
+      className={className}
+      style={style}
+      $width={width}
+      $collapsedWidth={collapsedWidth}
+    >
+      {backLinksWithIcons.length > 0 && (
+        <StyledSideEntityMenuHeader>
+          {backLinksWithIcons.map((link, index) => (
+            <SideEntityMenuItem
+              key={`backlink-${index}`}
+              link={link}
+              index={index}
+            />
+          ))}
+        </StyledSideEntityMenuHeader>
+      )}
+
+      {menuLinks.map((link, index) => (
+        <Fragment key={`menu-${index}`}>
+          <SideEntityMenuItem link={link} index={index} />
+          {link.pinned && index === 0 && <StyledPinnedSpacer />}
+        </Fragment>
+      ))}
+    </StyledSideEntityMenu>
+  );
+};
+
+export { SideEntityMenu };
+
+const StyledSideEntityMenu = styled.div<{
+  $width: string;
+  $collapsedWidth: string;
+}>`
+  ${sideEntityMenuStyles}
+  width: ${({ $collapsedWidth }) => $collapsedWidth};
+  transition: width 0.2s ease-in-out;
+
+  /* Show all when hovering any menu item */
+  &:has(button:hover, a:hover) {
+    width: ${({ $width }) => $width};
+
+    div[data-text-content] {
+      opacity: 1;
+      visibility: visible;
+    }
+  }
+`;
+
+const StyledSideEntityMenuHeader = styled.div`
+  ${sideEntityMenuHeaderStyles}
+`;
+
+const StyledPinnedSpacer = styled.div`
+  height: 1rem;
+`;
