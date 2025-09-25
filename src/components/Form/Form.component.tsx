b8705877bc93ad3a27d13452829b7afe84a28392
JN Veigel <blksnk@macbookair-2.home>
2025-09-25 12:01:12 +0200

    chore: bump component version

--- src/components/Form/Form.component.tsx
+++ src/components/Form/Form.component.tsx
@@ -55,7 +55,7 @@ const defaultFormProps: FormDefaultProps<object> = {
 /**
  * A flexible yet expressive form renderer.
  *
- * @version 0.0.35
+ * @version 0.0.36
  *
  * @template {object} TData - The type of the form data
  *
