45de0bed9e09d721ec7c7a1e39a6b8d56b7ffa34
JN Veigel <blksnk@macbookair-1.home>
2025-09-29 10:17:44 +0200

    feat: reflect select / search / multi-select option icon color in read mode form

--- src/components/Form/Form.component.tsx
+++ src/components/Form/Form.component.tsx
@@ -55,7 +55,7 @@ const defaultFormProps: FormDefaultProps<object> = {
 /**
  * A flexible yet expressive form renderer.
  *
- * @version 0.0.37
+ * @version 0.0.38
  *
  * @template {object} TData - The type of the form data
  *
