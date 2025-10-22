2632fd96d8ad5c0d966c07682d1fb1da77a558a4
blksnk <art.jnveigel@gmail.com>
2025-10-22 16:15:19 +0200

    feat: add ve2 type

--- src/types/global/index.ts
+++ src/types/global/index.ts
@@ -2,3 +2,4 @@ export type * from "./css.types";
 export type * from "./styles.types";
 export type * from "./data.types";
 export type * from "./direction.types";
+export type * from "./object.types";
