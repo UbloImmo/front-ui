80b883bb47fd088543b6d8527e1eb86a517c7a05
JN Veigel <blksnk@MacBook-Air-de-pg.local>
2026-01-12 18:02:36 +0100

    chore: version

--- src/components/Form/Form.component.tsx
+++ src/components/Form/Form.component.tsx
@@ -56,7 +56,7 @@ const defaultFormProps: FormDefaultProps<object> = {
 /**
  * A flexible yet expressive form renderer.
  *
- * @version 0.0.39
+ * @version 0.0.40
  *
  * @template {object} TData - The type of the form data
  *
