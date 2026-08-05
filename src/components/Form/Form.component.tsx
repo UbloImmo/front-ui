42a3f6d5c9a05266f81fe2ec20212b6f172cae95
blksnk <art.jnveigel@gmail.com>
2026-08-05 17:45:29 +0200

    chore(form): bump version

--- src/components/Form/Form.component.tsx
+++ src/components/Form/Form.component.tsx
@@ -54,7 +54,7 @@ const defaultFormProps: FormDefaultProps<object> = {
 /**
  * A flexible yet expressive form renderer.
  *
- * @version 0.1.2
+ * @version 0.1.3
  *
  * @template {object} TData - The type of the form data
  *
