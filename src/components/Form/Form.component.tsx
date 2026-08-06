c51344cbdaa9539ebe6d6343dcd06183be38a806
blksnk <art.jnveigel@gmail.com>
2026-08-06 15:06:43 +0200

    chore(form): bump major version following breaking change

--- src/components/Form/Form.component.tsx
+++ src/components/Form/Form.component.tsx
@@ -54,7 +54,7 @@ const defaultFormProps: FormDefaultProps<object> = {
 /**
  * A flexible yet expressive form renderer.
  *
- * @version 0.1.3
+ * @version 1.0.0
  *
  * @template {object} TData - The type of the form data
  *
