f95c17bf572711f9b9a5162270507cccf1e8954c
blksnk <art.jnveigel@gmail.com>
2026-09-02 12:36:51 +0200

    feat(TableCell): add role, onClick, position, title props

--- src/layouts/Table/components/TableCell/TableCell.component.tsx
+++ src/layouts/Table/components/TableCell/TableCell.component.tsx
@@ -2,7 +2,7 @@ import { forwardRef } from "react";
 
 import { useTableCellStyles } from "./TableCell.styles";
 
-import { useMergedProps, useTestId } from "@utils";
+import { useHtmlAttribute, useMergedProps, useTestId } from "@utils";
 
 import type { TableCellProps } from "./TableCell.types";
 import type { TestIdProps } from "@types";
@@ -15,12 +15,16 @@ const defaultTableCellProps: Required<TableCellProps> = {
   styleOverride: null,
   minWidth: null,
   fixedWidth: null,
+  role: "cell",
+  onClick: null,
+  position: null,
+  title: null,
 };
 
 /**
  * A table cell component. Used in `TableRow`.
  *
- * @version 0.1.1
+ * @version 0.1.2
  *
  * @param {CellProps} props - The props for the component.
  * @return {JSX.Element} The rendered table cell.
@@ -31,8 +35,12 @@ const TableCell = forwardRef<
 >((props: TableCellProps & TestIdProps, ref): JSX.Element => {
   const mergedProps = useMergedProps(defaultTableCellProps, props);
 
+  const onClick = useHtmlAttribute(mergedProps.onClick);
   const { className, style } = useTableCellStyles(mergedProps);
   const testId = useTestId("table-cell", props);
+
+  const position = useHtmlAttribute(mergedProps.position);
+  const title = useHtmlAttribute(mergedProps.title);
 
   return (
     <td
@@ -41,6 +49,10 @@ const TableCell = forwardRef<
       className={className}
       style={style}
       ref={ref}
+      role={mergedProps.role}
+      title={title}
+      onClick={onClick}
+      data-position={position}
     >
       {props.children}
     </td>
