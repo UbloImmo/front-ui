d0d6c566007d3c405643720531b34bd233243924
blksnk <art.jnveigel@gmail.com>
2026-09-02 12:33:49 +0200

    feat(types): add dom & axis global types

---
+++ src/types/global/dom.types.ts
@@ -0,0 +1,4 @@
+export type DOMRectValues = Pick<
+  DOMRectReadOnly,
+  "bottom" | "height" | "left" | "right" | "top" | "width"
+>;
--- src/types/global/index.ts
+++ src/types/global/index.ts
@@ -1,6 +1,8 @@
 export type * from "./aria.types";
+export type * from "./axis.types";
 export type * from "./css.types";
-export type * from "./styles.types";
 export type * from "./data.types";
 export type * from "./direction.types";
+export type * from "./dom.types";
 export type * from "./object.types";
+export type * from "./styles.types";
